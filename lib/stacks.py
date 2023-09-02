7d6f4b02ec2bd12d6deacd54e23ed3514d585c48
KenCorp <kennethwarui35@gmail.com>
2023-09-03 00:04:30 +0300

    Added sequence

--- lib/stacks.py
+++ lib/stacks.py
@@ -1,33 +1,28 @@
 class Data:
-
-
     def __init__(self, expression):
         self.expression = expression
         self.stack = []
 
     def is_balanced(self):
-
-        #  Setting uo the dictionaries to keep the objects inorder to iterate each 
-        data_structure = {')' : '(', '}':'{', '[' : ']', }
+        data_structure = {')': '(', '}': '{', ']': '['}
 
         for i in self.expression:
-            #  if the type is an opening bracket append it
             if i in data_structure.values():
                 self.stack.append(i)
-            #  if the type is a closing structure
             elif i in data_structure.keys():
-
                 if not self.stack or self.stack.pop() != data_structure[i]:
                     return False
         return not self.stack
-    
 
+    def remove_duplicates(self):
+        my_list = []
+        seen_elements = set()
 
+        for i in self.expression:
+            if i not in seen_elements:
+                my_list.append(i)
+                seen_elements.add(i)
 
-
-
-
-    
-    
+        return my_list
 
 
